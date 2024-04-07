99279f0418fa109165cd4db3dc4f57d35965a93a
mukul kasana <145048048+mukulkasana0001@users.noreply.github.com>
2024-04-07 09:47:13 +0000

    project one is done

--- project1_color_changer/chaiaurcode.js
+++ project1_color_changer/chaiaurcode.js
@@ -9,13 +9,13 @@ button.forEach( (button)=>{
         body.style.backgroundColor="grey";
      }
      if(e.target.id==="white"){
-        body.style.backgroundColor="grey";
+        body.style.backgroundColor="white";
      }
      if(e.target.id==="blue"){
-        body.style.backgroundColor="grey";
+        body.style.backgroundColor="blue";
      }
      if(e.target.id==="yellow"){
-        body.style.backgroundColor="grey";
+        body.style.backgroundColor="yellow";
      }
     })
 });
