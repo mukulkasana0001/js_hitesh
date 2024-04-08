337caba5631103c12e4fede5e9b4ace155eadf25
mukul kasana <145048048+mukulkasana0001@users.noreply.github.com>
2024-04-08 17:04:19 +0000

    not complete

---
+++ 10_project/chaiaurcode.js
@@ -0,0 +1,13 @@
+const randomnumber = parseInt(Math.random()*100+1);
+ const userinput=document.querySelector("#guessField");
+  const quess= parseInt(userinput.value)
+ console.log(quess)
+ const submit =document.querySelector("#subt");
+  const PreviousGuesses =document.querySelector(".guesses");
+   const lastResult =document.querySelector(".lastResult");
+    const lowOrHi =document.querySelector(".lowOrHi");
+
+
+  function validity(userinput){
+//  
+  }  
