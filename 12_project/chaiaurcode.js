bfc412d569eea7a23c6b82629dfb96f562a5e79c
mukul kasana <145048048+mukulkasana0001@users.noreply.github.com>
2024-04-11 15:11:24 +0000

    project 12 rendom color generator done

--- 12_project/chaiaurcode.js
+++ 12_project/chaiaurcode.js
@@ -1,14 +1,29 @@
 const start = document.querySelector("#start")
     const stop = document.querySelector("#stop")
     const  body =document.querySelector("body")
+    const randomcolor = function (){
+   
+        const hex ="0123456789ABCDEF"
+        let color = "#"
+        for(i=0 ; i<6 ;i++)
+        {
+            color +=hex[Math.floor(Math.random()*16)]
+            
+        }
+    
+        return color;
+    
+    }
+
+    console.log(randomcolor())
 
 
     start.addEventListener("click",function(){
          set()
     })
-
+   
     function set(){
-        const intervalID = setInterval(saydate,2000,"hello");
+        let intervalID = setInterval(saydate,1000,"started");
         stop.addEventListener("click",function(){
         dismis(intervalID);
     })
@@ -16,11 +31,12 @@ const start = document.querySelector("#start")
     
        function dismis(intervalID){
         clearInterval(intervalID);
-        
+        intervalID =null;
+        body.style.backgroundColor = randomcolor()
         console.log("stoped")
     }
     var saydate = function (str){
         
-        
+        body.style.backgroundColor = randomcolor()
         console.log(str)
     }
