bee00731cebf60a5b225f0836170a8613905ecbd
mukul kasana <mukulkasana0001@gmail.com>
2024-09-04 18:29:14 +0530

    call() and call() with this update

--- 15_classes_oop/call.js
+++ 15_classes_oop/call.js
@@ -4,7 +4,7 @@ function setusername(username){
 }
  
 function creatuser(username,password,email){
-    setusername.call(this ,username)
+    setusername.call(this ,username)   //CALL-> IT HOLDS THE REFERENCE OF THE FUNCTION ''  this -> we pass this as a first parameter with call() ( setusername() use the this of  creatuser() ) so when a setusername() is 'removed from stack ' it pass its CURRENT EXICUTION CONTEST to the  creatuser() 's  this
 // CALL-> IT HOLDS THE REFERENCE OF THE FUNCTION 
 // AND HERE this PASS THE CURRENT EXICUTION CONTEST TO THE function  
 
